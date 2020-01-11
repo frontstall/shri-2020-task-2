e7b21537f223b3f0378081ef8b9061c438b87dbe
frontstall <deepspiral@yandex.ru>
2020-01-11 20:41:34 +0300

    commit

--- src/validators/warning/index.js
+++ src/validators/warning/index.js
@@ -1,13 +1,13 @@
 import validate from '../../utils/validate';
-// import validateButtonPosition from './validateButtonPosition';
-// import validateButtonSize from './validateButtonSize';
-// import validateTextSize from './validateTextSize';
+import validateButtonPosition from './validateButtonPosition';
+import validateButtonSize from './validateButtonSize';
+import validateTextSize from './validateTextSize';
 // import validatePlaceholderSize from './validatePlaceholderSize';
 
 const validators = [
-  // validateButtonPosition,
-  // validateButtonSize,
-  // validateTextSize,
+  validateButtonPosition,
+  validateButtonSize,
+  validateTextSize,
   // validatePlaceholderSize,
 ];
 
