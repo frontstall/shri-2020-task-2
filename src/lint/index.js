aa307de21763cc316a8f1a8595be3bf147abc3d0
frontstall <deepspiral@yandex.ru>
2020-01-11 17:55:30 +0300

    remove try catch

--- src/lint/index.js
+++ src/lint/index.js
@@ -1,28 +1,26 @@
 import parse from 'json-to-ast';
-// import reduce from 'lodash/reduce';
+import reduce from 'lodash/reduce';
 
 import * as validators from '../validators';
 
 export default (json) => {
-  let ast;
+  const ast = parse(json);
+  // let ast;
 
-  try {
-    ast = parse(json);
-  } catch (error) {
-    console.error('Unable to parse data. Check JSON syntax.');
+  // try {
+  // } catch (error) {
+  //   console.error('Unable to parse data. Check JSON syntax.');
 
-    return [];
-  }
+  //   return [];
+  // }
 
-  return validators.validateHeaders(ast);
+  return reduce(
+    validators,
+    (result, validate) => {
+      const errors = validate(ast);
 
-  // return reduce(
-  //   validators,
-  //   (result, validate) => {
-  //     const errors = validate(ast);
-
-  //     return [...result, ...errors];
-  //   },
-  //   [],
-  // );
+      return [...result, ...errors];
+    },
+    [],
+  );
 };
